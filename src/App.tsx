20518e7e78ed3893a1c99d3f66562c50bcb501a7
Lucas Lacerda <contatollacerda@gmail.com>
2023-01-23 19:33:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-orange-500 text-3xl">Lucas</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        darkText: 'hsl(229, 25%, 31%)',
+        scoreText: 'hsl(229, 64%, 46%)',
+        headerOutline: 'hsl(217, 16%, 45%)',
+      },
+    },
+  },
+  plugins: [],
+};
